888ec513ff413b5d5bcbfef62550a9d5d534ef50
chentianyou <465072410@qq.com>
2018-07-25 14:30:24 +0000

    add setup env

---
+++ setup-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/opt/dependency/clang/bin:$PATH
+export LD_LIBRARY_PATH=/opt/dependency/clang/lib:/usr/lib64:/usr/lib:/usr/local/lib64:/usr/local/lib
+export CC=/opt/dependency/clang/bin/clang
+export CXX=/opt/dependency/clang/bin/clang++
